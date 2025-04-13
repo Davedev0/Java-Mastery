c9c6914867f3d138891aadd5da43fac75eb841c5
dev_ <jhondavefuentes9@gmail.com>
2025-04-13 23:41:01 +0800

    Create authenticationSimulation.java

---
+++ main/authenticationSimulation.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class authenticationSimulation {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        String[] userEmails = {"dave", "dev", "jhondave"};
+        String[] userPasswords = {"dave123", "dev000", "jhondave12345"};
+        
+        System.out.print("Enter your email: ");
+        String email = s.nextLine();
+        
+        System.out.print("Enter your password: ");
+        String password = s.nextLine();
+        boolean accountExist = false;
+        for(int i = 0; i < userEmails.length; i++){
+            if (email.equals(userEmails[i]) && password.equals(userPasswords[i])){
+                accountExist = true;
+                break;
+            }
+        }
+        if(accountExist) System.out.println("Welcome, " + email);
+        else System.out.println("Account Not Found!");
+    }
+                                            
+}
