9f837be3afe65876af33b943ba223f1755d34110
dev_ <jhondavefuentes9@gmail.com>
2025-05-09 00:34:16 +0800

    Create arraysIndex.java

---
+++ main/arrays/arraysIndex.java
@@ -0,0 +1,27 @@
+
+// Basic array index example
+public class arraysIndex {
+    public static void main(String[] args) {
+        String userNames[] = {
+            "daveDev", "daveFuentes01",
+            "jhondave0"
+        };
+            
+        String emails[] = {
+            "dave9@gmail.com",
+            "jhondave@gmail.com",
+            "pogi@gmail.com"
+        };
+        
+        String passwords[] = {
+            "123pogi", "palatuti0123",
+            "matt123"
+        };
+        
+        int i = 2;
+        System.out.println("Username: " + userNames[i]);
+        System.out.println("Email: " + emails[i]);
+        System.out.print("Password: " + passwords[i]);
+    }
+    
+}
