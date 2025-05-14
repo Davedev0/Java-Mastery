8f803eeb3dd37bccd1f3fcfb9983824fd9379b6f
dev_ <jhondavefuentes9@gmail.com>
2025-05-14 17:45:09 +0800

    Create variable.java

---
+++ main/variables/variable.java
@@ -0,0 +1,16 @@
+public class variable {
+    
+    public static void main(String[] args) {
+        String name = "Dave";
+        int age = 18;
+        char bloodType = 'O';
+        float gpa = 1.25f;
+        double size = 1.43;
+        System.out.println("Hello my name is " + name);
+        System.out.println("My age is " + age);
+        System.out.println("My Blood type is " + bloodType);
+        System.out.println("My GPA is " + gpa);
+        System.out.println("The size is " + size);
+    }
+    
+}
