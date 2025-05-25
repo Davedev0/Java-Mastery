ba4f88503ecd489cd5e1deb968aad0a42c185e23
dev_ <jhondavefuentes9@gmail.com>
2025-05-25 20:33:23 +0800

    Add files via upload

---
+++ main/loop/TryAndCatch.java
@@ -0,0 +1,28 @@
+package simpleCode;
+import java.util.Scanner;
+public class TryAndCatch {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean validInput = false;
+        int age = 0; 
+        
+        while (!validInput) {
+            try {
+                System.out.print("Enter your age: ");
+                age = scan.nextInt();
+                scan.nextLine(); 
+                
+                if (age < 10) System.out.println("Too young!");
+                else if (age > 80) System.out.println("Too old!");
+                System.out.println("Your age: " + age);
+                validInput = true;
+                
+            } catch (Exception e) {
+                System.out.println("Input numbers only!");
+                scan.nextLine();
+            }
+         }
+        
+        System.out.println("Program closed."); 
+    }
+}
